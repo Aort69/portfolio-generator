b1587a156e0700c57a41416d37f0089c97ee2f5c
Allec <allec.ortega69@Gmail.com>
2021-01-17 18:02:53 -0700

    first question posed

--- app.js
+++ app.js
@@ -9,4 +9,14 @@ const inquirer = require('inquirer');
 
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
-console.log(inquirer)
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      }
+    ]);
+  };
+  
+  promptUser().then(answers => console.log(answers));
